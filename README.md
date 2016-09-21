b309e01b6422019e6974a8925e18846125d0ed5f
Wes Ruvalcaba <wesruv@gmail.com>
2016-09-21 13:56:51 -0400

    Updated with code from initial release in core

--- README.md
+++ README.md
@@ -1,21 +1,2 @@
 # Basis Theme
-Contrib version of core Backdrop theme.
-
-This repo is the playground version of the core theme, for trying things, adding expiremental features, and working outside of some of the contraints of core.
-
-#### Feel free to create issues!
-I'd love to get any feedback, features, or issues anyone has while messing around with theme.
-
-####Feel free to create pull requests!
-But only merge them if you've been made a contributor
-
-### Resources
-* [See issue #1361 for more information](https://github.com/backdrop/backdrop-issues/issues/1361)
-* [See the most recent mockups](https://github.com/backdrop/backdrop-issues/issues/1361#issuecomment-244248246), [and some here](https://github.com/backdrop/backdrop-issues/issues/1361#issuecomment-185977384)
-
-### Contributors
-* [Wes Ruvalcaba](https://github.com/wesruv)
-* (Looking for more!)
-
-### Mockup
-![Mockup of a homepage using this theme](https://cloud.githubusercontent.com/assets/5607236/18188316/35d41a54-707f-11e6-8bcb-a81c5376cd40.png)
+Default theme in Backdrop.
--- templates/header.tpl.php
+++ templates/header.tpl.php
@@ -8,34 +8,17 @@
  * - $base_path: The base URL path of the Backdrop installation. At the very
  *   least, this will always default to /.
  * - $directory: The directory the template is located in, e.g. modules/system
- *   or themes/bartik.
+ *   or themes/basis/templates.
  * - $is_front: TRUE if the current page is the front page.
  * - $logged_in: TRUE if the user is registered and signed in.
- * - $logo: The path to the logo image, as defined in theme configuration.
- * - $front_page: The URL of the front page. Use this instead of $base_path, when
- *   linking to the front page. This includes the language domain or prefix.
+ * - $front_page: The URL of the front page. Use this instead of $base_path when
+ *   linking to the front page since it includes the language domain or prefix.
+ *
+ * - $logo: The path to the logo image, as defined in site configuration.
  * - $site_name: The name of the site, empty when display has been disabled.
  * - $site_slogan: The site slogan, empty when display has been disabled.
  * - $menu: The menu for the header (if any), as an HTML string.
  */
-?>
-
-<?php
-/**
- * Add class for tall or wide logo
- * @todo  tried doing this code in template_preprocess_header, new index in variables
- * didn't make it into this template :(
- */
-$header_logo_classes = '';
-
-if (!empty($logo)) {
-  $logo_size = getimagesize($logo);
-  if (!empty($logo_size)) {
-    if ($logo_size[0] < $logo_size[1]) {
-      $header_logo_classes = ' header-logo-tall';
-    }
-  }
-}
 ?>
 
 <?php if ($site_name || $site_slogan || $logo): ?>
@@ -44,13 +27,13 @@ if (!empty($logo)) {
         <?php // Strong class only added for semantic value ?>
         <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header-site-name-link" rel="home">
           <?php if ($logo): ?>
-            <div class="header-logo-wrapper<?php print $header_logo_classes; ?>">
-              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header-logo" />
+            <div class="<?php print implode(' ', $logo_wrapper_classes); ?>">
+              <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header-logo" <?php print backdrop_attributes($logo_attributes); ?> />
             </div>
           <?php endif; ?>
-          <strong class="semantic">
-            <?php print $site_name; ?>
-          </strong>
+          <?php if ($site_name): ?>
+            <strong><?php print $site_name; ?></strong>
+          <?php endif; ?>
         </a>
       </div>
       <?php if ($site_slogan): ?>
